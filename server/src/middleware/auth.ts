f545bd013ba98ebc9ccdb6b192ac427634a66253
Celeste Hayes <celestehayes@Mac.attlocal.net>
2025-01-22 21:57:03 -0500

    Refactor: add middleware auth.ts

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -6,5 +6,23 @@ interface JwtPayload {
 }
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    // Get the token
+    const token = authHeader.split(' ')[1];
+    // Get the secret key
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403).json({ message: 'Token invalid' });
+      }
+      req.user = user as JwtPayload;
+      return next();
+    });
+  } else {
+    res.sendStatus(401).json({ message: 'Unauthorized'});
+  }
 };
