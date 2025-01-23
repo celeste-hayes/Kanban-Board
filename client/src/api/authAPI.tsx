50402c52e27f7e6c3a9cafb667573da092b0f66e
Celeste Hayes <celestehayes@Mac.attlocal.net>
2025-01-22 22:12:04 -0500

    Refactor: add client side authAPI

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,18 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
+  try {
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userInfo)
+    });
+
+    return response;  // Return data from server
+  } catch (err) {
+    console.log('Error from user login: ', err);
+  }
 }
-
-
-
 export { login };
